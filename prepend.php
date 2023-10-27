790285db208b62b23c5b4da4fd5bad7af741e48a
Stephen J. Carnam <steve@steveorevo.com>
2023-10-26 17:27:43 -0700

    wip

--- prepend.php
+++ prepend.php
@@ -1,6 +1,6 @@
 <?php
  if ( ! class_exists( 'WP_Global_Runtime') ) {
-    class WP_Global_Runtime() {
+    class WP_Global_Runtime {
         public function __construct() {
             if ( ! isset( $_SERVER['HOME'] ) ) return;
 
